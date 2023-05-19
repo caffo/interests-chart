d02be9ad5ba110ee06644806036635d3aebffd1e
Rodrigo Franco <caffo@users.noreply.github.com>
2023-05-18 23:41:29 -0300

    Update calendarTemplate.js

--- templates/calendarTemplate.js
+++ templates/calendarTemplate.js
@@ -181,15 +181,8 @@ exports.templateBottom = `
         text-align: center !important;
     }
     
-    @media(hover: hover) and (pointer: fine) {
-    .day > .itooltip { 
-            display: flex;
-            flex-direction: column;
-            align-items: flex-start;
-        }
-    }
     
-    .day:hover > .itooltip, .day:active > .itooltip, .day:focus > .itooltip { 
+    .day:hover>.itooltip
         display: flex;
         flex-direction: column;
         align-items: flex-start;
